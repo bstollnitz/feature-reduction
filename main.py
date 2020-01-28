a524b479543cf7587c510dda476dfa3fc2a02b2d
Bea Stollnitz <bea@stollnitz.com>
2020-01-28 11:37:51 -0800

    Initial plumbing for project.

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+from typing import List, Tuple
+
+import numpy as np
+import plotly.graph_objects as go
+import plotly.io as pio
+from plotly.subplots import make_subplots
+
+import utils
+
+S3_URL = 'https://bea-portfolio.s3-us-west-2.amazonaws.com/feature-reduction/'
+PLOTS_FOLDER = 'plots'
+COLOR1 = '#3F4F8C'
+
+
+def main() -> None:
+    """
+    Main program.
+    """
+        
+    print('Feature reduction')
+
+if __name__ == '__main__':
+    main()
