6f8e3987748687ea063333d2484572f7e7158730
Bea Stollnitz <bea@stollnitz.com>
2020-01-28 13:12:43 -0800

    Load data from S3.

--- main.py
+++ main.py
@@ -5,20 +5,50 @@ import numpy as np
 import plotly.graph_objects as go
 import plotly.io as pio
 from plotly.subplots import make_subplots
+from scipy.io import loadmat
 
 import utils
 
 S3_URL = 'https://bea-portfolio.s3-us-west-2.amazonaws.com/feature-reduction/'
-PLOTS_FOLDER = 'plots'
 COLOR1 = '#3F4F8C'
+
+
+def load_data(cam_index: int) -> List[np.ndarray]:
+    """
+    Creates a 'data' folder if it doesn't exist and loads data for this project 
+    if it's not yet present in the 'data' folder. Returns the loaded data.
+    """
+
+    # Download mat files from Amazon S3 if they're not yet local.
+    index_list = [1, 2, 3]
+    filenames = [f'cam{cam_index}_{j}.mat' for j in index_list]
+    local_paths = []
+    for filename in filenames:
+        remote_url = S3_URL + filename
+        local_path = utils.download_remote_data_file(remote_url)
+        local_paths.append(local_path)
+
+    # Load each data file into memory.
+    data_files = [loadmat(local_path, struct_as_record=False) for local_path 
+        in local_paths]
+    
+    # Extract information for each data files into an array.
+    # Each arrary is of size 480 x 640 x 3 x 226. This corresponds to 226 
+    # color images, of size 480 x 640.
+    data_list = [data_files[j-1][f'vidFrames{cam_index}_{j}'] for j in index_list]
+
+    return data_list
 
 
 def main() -> None:
     """
     Main program.
     """
-        
-    print('Feature reduction')
+    
+    print('Main')
+    plots_dir_path = utils.find_or_create_plots()
+    load_data(1)
+
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -6,10 +6,10 @@ from urllib.request import urlopen
 
 import numpy as np
 
-DATA_FOLDER = "data"
+DATA_FOLDER = 'data'
+PLOTS_FOLDER = 'plots'
 
-
-def find_or_create_dir(dir_name: str) -> str:
+def _find_or_create_dir(dir_name: str) -> str:
     """
     Creates a directory if it doesn't exist.
     """
@@ -25,6 +25,14 @@ def find_or_create_dir(dir_name: str) -> str:
     return dir_path
 
 
+def find_or_create_plots() -> str:
+    """
+    Creates a 'plots' directory if it doesn't exist.
+    """
+
+    return _find_or_create_dir(PLOTS_FOLDER)
+
+
 def download_remote_data_file(data_url: str) -> str:
     """
     Gets the data from url if it's not saved locally yet.
@@ -32,18 +40,17 @@ def download_remote_data_file(data_url: str) -> str:
     """
     
     # Create a data directory if it doesn't exist.
-    data_dir_path = find_or_create_dir(DATA_FOLDER)
+    data_dir_path = _find_or_create_dir(DATA_FOLDER)
     
     # Download the data file if it doesn't exist.
     filename = os.path.basename(urlparse(data_url).path)
     data_file_path = os.path.join(data_dir_path, filename)
-    print(f"data_file_path: {data_file_path}")
     if not os.path.exists(data_file_path):
-        print("Downloading data file...")
+        print(f'Downloading data file {data_file_path}...')
         with urlopen(data_url) as response:
             with open(data_file_path, "wb") as data_file:
                 shutil.copyfileobj(response, data_file)
-        print("Done downloading data file.")
+        print('Done downloading data file.')
 
     return data_file_path
 
